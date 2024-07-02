771677dac920b095612d881da3d7f9c2cc4b2fa8
Akashdeepgupta124 <134071390+Akashdeepgupta124@users.noreply.github.com>
2024-07-02 23:03:23 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Hero() {
+    return (
+        <main className='hero'>
+            <div className="hero_content">
+                <h1>
+                YOUR FEET 
+DESERVE
+THE BEST
+                </h1>
+                <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+                <div className="button_container">
+                <button id='red_btn'>Shop Now</button>
+                <button id='white_btn'>Category</button>
+            </div>
+            </div>
+           
+
+
+            <div className="hero_image">
+                <img src="\images\shoe_image.png" alt="" />
+            </div>
+
+        </main>
+    )
+}
+
+export default Hero
